df9b92a22be5f93db7fac83f64718810ac674bc8
Nucleus Erumagborie <nucleuserumagborie@gmail.com>
2024-05-12 21:47:12 +0100

    Projects Layout Update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,21 +7,21 @@ export default function Projects() {
     <section id="projects" className="py-16">
       <h2 className="text-2xl sm:text-3xl font-bold mb-8">Projects</h2>
 
-      <div className="grid md:grid-cols-[1fr_1fr] gap-8">
+      <div className="projects">
         {projects.map((project) => (
           <div
             key={project.name}
-            className="grid grid-rows-[auto_1fr] sm:grid-rows-1 sm:grid-cols-[auto_1fr] gap-4"
+            className=""
           >
             <img
               src={project.preview}
               alt="preview"
-              className="w-32 h-20 border-1 object-cover border-gray-700 rounded"
+              className="w-32 h-20 border-1 object-cover border-gray-700 rounded mb-4"
             />
 
             <div className="flex flex-col gap-2">
               <div className="font-bold text-lg">{project.name}</div>
-              <div className="text-gray-300">{project.description}</div>
+              <div className="text-gray-300 line-clamp-3">{project.description}</div>
               <div className="flex flex-wrap gap-2">
                 {project.tools.map((tool) => (
                   <div className="tool text-sm pt-1 pb-1.5 px-4 font-medium rounded-full text-blue-600">
