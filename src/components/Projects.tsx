2fa52f6a6a44b4fe868b0d847e6ebbcb15bd9104
Nucleus Erumagborie <nucleuserumagborie@gmail.com>
2024-05-13 06:58:53 +0100

    J IYKE Forx Portal

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,10 +9,7 @@ export default function Projects() {
 
       <div className="projects">
         {projects.map((project) => (
-          <div
-            key={project.name}
-            className=""
-          >
+          <div key={project.name} className="">
             <img
               src={project.preview}
               alt="preview"
@@ -21,7 +18,9 @@ export default function Projects() {
 
             <div className="flex flex-col gap-2">
               <div className="font-bold text-lg">{project.name}</div>
-              <div className="text-gray-300 line-clamp-3">{project.description}</div>
+              <div className="text-gray-300 line-clamp-3">
+                {project.description}
+              </div>
               <div className="flex flex-wrap gap-2">
                 {project.tools.map((tool) => (
                   <div className="tool text-sm pt-1 pb-1.5 px-4 font-medium rounded-full text-blue-600">
@@ -30,10 +29,12 @@ export default function Projects() {
                 ))}
               </div>
               <div className="text-sm font-medium text-gray-400 flex gap-4">
-                <a href={project.source} className="flex items-center gap-1">
-                  <img className="w-4" src={Github} alt="globe" />
-                  <span>Source</span>
-                </a>
+                {project.source && (
+                  <a href={project.source} className="flex items-center gap-1">
+                    <img className="w-4" src={Github} alt="globe" />
+                    <span>Source</span>
+                  </a>
+                )}
                 <a href={project.url} className="flex items-center gap-1">
                   <img className="w-4" src={Globe} alt="globe" />
                   <span>Live</span>
