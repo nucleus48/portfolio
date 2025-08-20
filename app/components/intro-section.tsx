3e3de3061e797f246dabda449ac7c84746067ee5
nucleus48 <nucleuserumagborie@gmail.com>
2025-08-20 09:52:44 +0100

    change skill to Fullstack Typescript Developer

--- app/components/intro-section.tsx
+++ app/components/intro-section.tsx
@@ -8,7 +8,7 @@ export default function IntroSection() {
       <h1 className="font-bold text-3xl mb-2 sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl">
         Nucleus Erumagborie
       </h1>
-      <h2 className="font-semibold mb-4">Software Engineer</h2>
+      <h2 className="font-semibold mb-4">Fullstack Typescript Developer</h2>
       <p className="mb-8 max-w-md">
         I build fast, reliable web and mobile apps with a strong focus on user
         experience and long-term scalability.
